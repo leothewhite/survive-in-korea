2a3da1e171d1d17e0c65523676ae56e7396a7e41
leothewhite <leothewhite091021@gmail.com>
2023-07-30 14:17:32 +0900

    refactor: clean code

--- main.py
+++ main.py
@@ -7,51 +7,51 @@ from sprites import *
 pygame.init()
 
 
-size_x, size_y = 640, 480
 player_x = 320
 to_x = 0
 
-player_img = pygame.image.load("./resources/images/character/player.png")
-border_img = pygame.image.load("./resources/images/background/border.png")
-background = pygame.image.load("./resources/images/background/background.png")
-
 place_path = "./resources/images/place/"
-place_image_list = os.listdir(place_path)
+images = {
+    "player": pygame.image.load("./resources/images/character/player.png"),
+    "border": pygame.image.load("./resources/images/background/border.png"),
+    "background": pygame.image.load("./resources/images/background/background.png"),
+    "place": [(i, pygame.image.load(place_path + i)) for i in os.listdir(place_path)],
+}
 
 down_left, down_right = False, False
 
 all_sprites = pygame.sprite.Group()
 
-SCREEN = pygame.display.set_mode((size_x, size_y))
+SCREEN = pygame.display.set_mode((640, 480))
 CLOCK = pygame.time.Clock()
 RUNNING = True
 
 speed = 0.2
 
+# left1, left2, right1, right2
 borders = [[], [], [], []]
-place_list = [[], [], []]
+
+# a: stress, b: study, c: dream
+places = {"a": [], "b": [], "c": []}
 
 
-player = Character(player_img)
+player = Character(images["player"])
 
 for i in range(32):
     now_y = i * 40
-    block = [
-        Border(border_img, (214, now_y)),
-        Border(border_img, (214, size_y + now_y)),
-        Border(border_img, (614, now_y)),
-        Border(border_img, (614, size_y + now_y)),
+    blocks = [
+        Border(images["border"], (214, now_y)),
+        Border(images["border"], (214, 480 + now_y)),
+        Border(images["border"], (614, now_y)),
+        Border(images["border"], (614, 480 + now_y)),
     ]
-    for i in range(4):
-        borders[i].append(block[i])
-        all_sprites.add(block[i])
+    for idx, block in enumerate(blocks):
+        borders[idx].append(block)
+        all_sprites.add(block)
 
-for i, v in enumerate(place_image_list):
-    file_name = v.split(".")[0]
-    # a: 스트레스 해소, 0번 인덱스, b: 공부, 1번 인덱스, c: 진로, 2번 인덱스
-    place_list[ord(v[0]) - 97].append(
-        Place(pygame.image.load(place_path + v), file_name[2:], (33, size_y))
-    )
+for idx, file in enumerate(images["place"]):
+    file_name = file[0].split(".")
+    places[file[0][0]].append(Place(file[1], file[0][2:], (33, 480)))
 
 
 place_now = [-1, -1, -1]
@@ -63,19 +63,22 @@ all_sprites.add(player)
 bg_y = 0
 
 
-def move_background():
+def background_manager():
     global bg_y, place_idx
-    SCREEN.blit(background, (0, bg_y))
-    SCREEN.blit(background, (0, 640 + bg_y))
+    SCREEN.blit(images["background"], (0, bg_y))
+    SCREEN.blit(images["background"], (0, 640 + bg_y))
+
     for idx in range(32):
         if 12 <= idx <= 16:
             if 2 < place_idx:
                 for i in range(3):
-                    place_now[i] = random.choice(place_list[i])
-                random.shuffle(place_list)
+                    place_now[i] = random.choice(places[chr(i + 97)])
+                random.shuffle(place_now)
                 place_idx = 0
-            place_now[place_idx].update((33, bg_y + size_y))
+
+            place_now[place_idx].update((33, bg_y + 480))
             place_now[place_idx].draw(SCREEN)
+
             for t in range(2, 4):
                 now = borders[t][idx]
                 now.update((now.rect.x, bg_y + idx * 40))
@@ -84,7 +87,7 @@ def move_background():
                 now = borders[t][idx]
                 now.update((now.rect.x, bg_y + idx * 40))
 
-    if bg_y <= -size_y - 200:
+    if bg_y <= -680:
         place_idx += 1
         bg_y = 0
 
@@ -109,8 +112,8 @@ def input_manager(event):
             down_right = False
 
 
-def chk_collide(a, b):
-    if pygame.Rect.collidelist(a, b) != -1:
+def collision_manager(a, b):
+    if pygame.Rect.collidelist(a, b) == -1:
         return False
     else:
         return True
@@ -123,17 +126,22 @@ while RUNNING:
         if event.type == pygame.QUIT:
             RUNNING = False
         input_manager(event)
+
     if down_left:
         to_x -= speed * dt
     if down_right:
         to_x += speed * dt
 
-    move_background()
+    background_manager()
 
-    if chk_collide(player.rect, borders[0]) or chk_collide(player.rect, borders[1]):
+    if collision_manager(player.rect, borders[0]) or collision_manager(
+        player.rect, borders[1]
+    ):
         if to_x < 0:
             to_x = 0
-    elif chk_collide(player.rect, borders[2]) or chk_collide(player.rect, borders[3]):
+    elif collision_manager(player.rect, borders[2]) or collision_manager(
+        player.rect, borders[3]
+    ):
         if 0 < to_x:
             to_x = 0
 
--- sprites.py
+++ sprites.py
@@ -1,15 +1,13 @@
 import pygame
+from typing import *
 
 
-# Player 객체를 정의한다
 class Character(pygame.sprite.Sprite):
-    # 처음 생성 시 img만 받아서 생성
     def __init__(self, img):
         pygame.sprite.Sprite.__init__(self)
         self.image = img
         self.rect = self.image.get_rect()
 
-    # position 값을 받아서 움직이기
     def update(self, pos):
         self.rect = self.image.get_rect()
         self.rect.topleft = pos
