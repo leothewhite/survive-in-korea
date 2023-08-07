35c8a805a1f0991a3b540a991445ee787bca4821
leothewhite <leothewhite091021@gmail.com>
2023-08-08 02:25:28 +0900

    refactor: seperate everything

--- main.py
+++ main.py
@@ -1,203 +1,58 @@
 import pygame
 import os
-import random
 
 from property import Character, Border, Place, Guage, ManageVariable
-from manager import move_background
+from manager import (
+    background_manager,
+    guage_manager,
+    input_manager,
+    collide_manager,
+    load_border,
+    load_images,
+    make_variable,
+)
 
 pygame.init()
 
 intl = ManageVariable()
 
-intl.size_x, intl.size_y = 640, 480
-player_x = 320
-to_x = 0
-gravity = 1
-
-place_path = "./resources/images/place/"
-guage_path = "./resources/images/guage/"
-intl.images = {
-    "player": pygame.image.load("./resources/images/character/player.png"),
-    "border": pygame.image.load("./resources/images/background/border.png"),
-    "background": pygame.image.load("./resources/images/background/background.png"),
-    "place": [
-        (i.split(".")[0], pygame.image.load(place_path + i))
-        for i in os.listdir(place_path)
-    ],
-    "guage": {
-        i.split(".")[0]: pygame.image.load(guage_path + i)
-        for i in os.listdir(guage_path)
-        if i != ".DS_Store"
-    },
-}
-
-guage = Guage()
-
-down_left, down_right = False, False
-
-all_sprites = pygame.sprite.Group()
+make_variable()
+load_images()
+load_border()
 
 
-intl.SCREEN = pygame.display.set_mode((intl.size_x, intl.size_y))
-CLOCK = pygame.time.Clock()
-RUNNING = True
+intl.player = Character(intl.images["player"])
+intl.place_now = intl.places["a"][0]
 
-speed = 0.2
-
-intl.borders = [[], [], [], []]
-# a: 놀기 b: 공부 c: 취미
-intl.places = {"a": [], "b": [], "c": []}
-place_cnt = {}
-
-player = Character(intl.images["player"])
-
-for i in range(32):
-    now_y = i * 60
-    block = [
-        Border(intl.images["border"], (171, now_y)),
-        Border(intl.images["border"], (171, intl.size_y + now_y)),
-        Border(intl.images["border"], (456, now_y)),
-        Border(intl.images["border"], (456, intl.size_y + now_y)),
-    ]
-    for i in range(4):
-        intl.borders[i].append(block[i])
-        all_sprites.add(block[i])
-
-for i, v in enumerate(intl.images["place"]):
-    file_name = v[0]
-    file = v[1]
-    intl.places[v[0][0]].append(Place(file, file_name[2:], (33, intl.size_y)))
-    place_cnt[file_name[2:]] = 0
+intl.player.update((intl.player_x, 240))
+intl.all_sprites.add(intl.player)
 
 
-intl.place_now_set = [-1, -1, -1]
-place_now = intl.places["a"][0]
-intl.place_idx = 3
-
-player.update((player_x, 240))
-all_sprites.add(player)
-
-intl.bg_y = 0
-
-
-def input_manager(event):
-    global to_x, down_left, down_right
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            to_x -= speed * 3 * intl.dt
-            down_left = True
-
-        if event.key == pygame.K_RIGHT:
-            to_x += speed * 3 * intl.dt
-            down_right = True
-
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT:
-            down_left = False
-        if event.key == pygame.K_RIGHT:
-            down_right = False
-
-
-def chk_collide(a, b):
-    return pygame.Rect.collidelist(a, b)
-
-
-def place_collide(player, place_type):
-    col = chk_collide(player.rect, intl.places[place_type]) + 1
-    if col:
-        return intl.places[place_type][col - 1].name
-    else:
-        return False
-
-
-def draw_guage():
-    intl.SCREEN.blit(intl.images["guage"]["frame"], (525, 440))
-    intl.SCREEN.blit(intl.images["guage"]["frame"], (525, 400))
-    intl.SCREEN.blit(intl.images["guage"]["frame"], (525, 360))
-    intl.SCREEN.blit(intl.images["guage"]["frame"], (525, 320))
-
-    for i in range(20):
-        if guage.health // 5 <= i:
-            break
-        intl.SCREEN.blit(intl.images["guage"]["health"], (527 + 5 * i, 322))
-
-    for i in range(20):
-        if guage.future // 5 <= i:
-            break
-        intl.SCREEN.blit(intl.images["guage"]["future"], (527 + 5 * i, 362))
-
-    for i in range(20):
-        if guage.stress // 5 <= i:
-            break
-        intl.SCREEN.blit(intl.images["guage"]["stress"], (527 + 5 * i, 402))
-
-    for i in range(20):
-        if guage.grade // 5 <= i:
-            break
-        intl.SCREEN.blit(intl.images["guage"]["grade"], (527 + 5 * i, 442))
-
-
-while RUNNING:
-    intl.dt = CLOCK.tick(60)
+while intl.RUNNING:
+    intl.dt = intl.CLOCK.tick(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            RUNNING = False
+            intl.RUNNING = False
         input_manager(event)
-    if down_left:
-        to_x -= speed * intl.dt
-    if down_right:
-        to_x += speed * intl.dt
 
-    move_background()
+    if intl.down_left:
+        intl.to_x -= intl.speed * intl.dt
+    if intl.down_right:
+        intl.to_x += intl.speed * intl.dt
 
-    if place_now.rect.y - 40 <= player.rect.y <= place_now.rect.y + 200:
-        to_x -= gravity
+    if intl.place_now.rect.y - 40 <= intl.player.rect.y <= intl.place_now.rect.y + 200:
+        intl.to_x -= intl.gravity
 
-    if (
-        chk_collide(player.rect, intl.borders[0]) + 1
-        or chk_collide(player.rect, intl.borders[1]) + 1
-    ):
-        if to_x < 0:
-            to_x = 0
-    elif (
-        chk_collide(player.rect, intl.borders[2]) + 1
-        or chk_collide(player.rect, intl.borders[3]) + 1
-    ):
-        if 0 < to_x:
-            to_x = 0
+    background_manager()
+    collide_manager()
+    guage_manager()
 
-    # # 스트레스 풀기
-    col_place = place_collide(player, "a")
-    if col_place:
-        guage.stress -= 5
-        place_cnt[col_place] += 1
-        if col_place == "alley":
-            guage.health -= 5
-        if col_place == "basketball":
-            guage.health += 5
-        if 3 <= place_cnt[col_place]:
-            guage.grade -= 5
+    intl.player_x += intl.to_x
 
-        player_x = 320
-
-    col_place = place_collide(player, "b")
-    if col_place:
-        guage.stress += 5
-        guage.grade += 5
-        player_x = 320
-
-    col_place = place_collide(player, "c")
-    if col_place:
-        guage.stress -= 5
-        guage.future += 5
-        player_x = 320
-
-    draw_guage()
-    player_x += to_x
-    player.update((player_x, 240))
-    all_sprites.draw(intl.SCREEN)
-    to_x = 0
+    intl.player.update((intl.player_x, 240))
+    intl.all_sprites.draw(intl.SCREEN)
+    intl.to_x = 0
 
     pygame.display.update()
 
--- manager.py
+++ manager.py
@@ -1,10 +1,57 @@
 import random
-from property import ManageVariable
+import os
+import pygame
+from property import ManageVariable, Border, Place, Guage
 
 intl = ManageVariable()
 
 
-def move_background():
+def make_variable():
+    intl.size_x, intl.size_y = 640, 480
+    intl.player_x = 320
+    intl.to_x = 0
+    intl.gravity = 1
+    intl.guage = Guage()
+    intl.down_left, intl.down_right = False, False
+    intl.all_sprites = pygame.sprite.Group()
+    intl.SCREEN = pygame.display.set_mode((intl.size_x, intl.size_y))
+    intl.CLOCK = pygame.time.Clock()
+    intl.RUNNING = True
+    intl.speed = 0.2
+    intl.borders = [[], [], [], []]
+    intl.places = {"a": [], "b": [], "c": []}
+    intl.place_cnt = {}
+    intl.bg_y = 0
+    intl.place_now_set = [-1, -1, -1]
+    intl.place_idx = 3
+
+
+def load_images():
+    intl.place_path = "./resources/images/place/"
+    intl.guage_path = "./resources/images/guage/"
+    intl.images = {
+        "player": pygame.image.load("./resources/images/character/player.png"),
+        "border": pygame.image.load("./resources/images/background/border.png"),
+        "background": pygame.image.load("./resources/images/background/background.png"),
+        "place": [
+            (i.split(".")[0], pygame.image.load(intl.place_path + i))
+            for i in os.listdir(intl.place_path)
+        ],
+        "guage": {
+            i.split(".")[0]: pygame.image.load(intl.guage_path + i)
+            for i in os.listdir(intl.guage_path)
+            if i != ".DS_Store"
+        },
+    }
+
+    for _, v in enumerate(intl.images["place"]):
+        file_name = v[0]
+        file = v[1]
+        intl.places[v[0][0]].append(Place(file, file_name[2:], (33, intl.size_y)))
+        intl.place_cnt[file_name[2:]] = 0
+
+
+def background_manager():
     intl.SCREEN.blit(intl.images["background"], (0, intl.bg_y))
     intl.SCREEN.blit(intl.images["background"], (0, 640 + intl.bg_y))
     for idx in range(32):
@@ -14,9 +61,9 @@ def move_background():
                     intl.place_now_set[i] = random.choice(intl.places[chr(i + 97)])
                 random.shuffle(intl.place_now_set)
                 intl.place_idx = 0
-            place_now = intl.place_now_set[intl.place_idx]
-            place_now.update((31, intl.bg_y + intl.size_y))
-            place_now.draw(intl.SCREEN)
+            intl.place_now = intl.place_now_set[intl.place_idx]
+            intl.place_now.update((31, intl.bg_y + intl.size_y))
+            intl.place_now.draw(intl.SCREEN)
             for t in range(2, 4):
                 now = intl.borders[t][idx]
                 now.update((now.rect.x, intl.bg_y + idx * 40))
@@ -26,6 +73,117 @@ def move_background():
                 now.update((now.rect.x, intl.bg_y + idx * 40))
     if intl.bg_y <= -intl.size_y - 160:
         intl.place_idx += 1
-        bg_y = 0
+        intl.bg_y = 0
 
     intl.bg_y -= 0.15 * intl.dt
+
+
+def guage_manager():
+    intl.SCREEN.blit(intl.images["guage"]["frame"], (525, 440))
+    intl.SCREEN.blit(intl.images["guage"]["frame"], (525, 400))
+    intl.SCREEN.blit(intl.images["guage"]["frame"], (525, 360))
+    intl.SCREEN.blit(intl.images["guage"]["frame"], (525, 320))
+
+    for i in range(20):
+        if intl.guage.health // 5 <= i:
+            break
+        intl.SCREEN.blit(intl.images["guage"]["health"], (527 + 5 * i, 322))
+
+    for i in range(20):
+        if intl.guage.future // 5 <= i:
+            break
+        intl.SCREEN.blit(intl.images["guage"]["future"], (527 + 5 * i, 362))
+
+    for i in range(20):
+        if intl.guage.stress // 5 <= i:
+            break
+        intl.SCREEN.blit(intl.images["guage"]["stress"], (527 + 5 * i, 402))
+
+    for i in range(20):
+        if intl.guage.grade // 5 <= i:
+            break
+        intl.SCREEN.blit(intl.images["guage"]["grade"], (527 + 5 * i, 442))
+
+
+def input_manager(event):
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            intl.to_x -= intl.speed * 3 * intl.dt
+            intl.down_left = True
+
+        if event.key == pygame.K_RIGHT:
+            intl.to_x += intl.speed * 3 * intl.dt
+            intl.down_right = True
+
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT:
+            intl.down_left = False
+        if event.key == pygame.K_RIGHT:
+            intl.down_right = False
+
+
+def chk_collide(a, b):
+    return pygame.Rect.collidelist(a, b)
+
+
+def place_collide(player, place_type):
+    col = chk_collide(player.rect, intl.places[place_type]) + 1
+    if col:
+        return intl.places[place_type][col - 1].name
+    else:
+        return False
+
+
+def collide_manager():
+    if (
+        chk_collide(intl.player.rect, intl.borders[0]) + 1
+        or chk_collide(intl.player.rect, intl.borders[1]) + 1
+    ):
+        if intl.to_x < 0:
+            intl.to_x = 0
+    elif (
+        chk_collide(intl.player.rect, intl.borders[2]) + 1
+        or chk_collide(intl.player.rect, intl.borders[3]) + 1
+    ):
+        if 0 < intl.to_x:
+            intl.to_x = 0
+
+    # # 스트레스 풀기
+    col_place = place_collide(intl.player, "a")
+    if col_place:
+        intl.guage.stress -= 5
+        intl.place_cnt[col_place] += 1
+        if col_place == "alley":
+            intl.guage.health -= 5
+        if col_place == "basketball":
+            intl.guage.health += 5
+        if 3 <= intl.place_cnt[col_place]:
+            intl.guage.grade -= 5
+
+        intl.player_x = 320
+
+    col_place = place_collide(intl.player, "b")
+    if col_place:
+        intl.guage.stress += 5
+        intl.guage.grade += 5
+        intl.player_x = 320
+
+    col_place = place_collide(intl.player, "c")
+    if col_place:
+        intl.guage.stress -= 5
+        intl.guage.future += 5
+        intl.player_x = 320
+
+
+def load_border():
+    for i in range(32):
+        now_y = i * 60
+        intl.block = [
+            Border(intl.images["border"], (171, now_y)),
+            Border(intl.images["border"], (171, intl.size_y + now_y)),
+            Border(intl.images["border"], (456, now_y)),
+            Border(intl.images["border"], (456, intl.size_y + now_y)),
+        ]
+        for i in range(4):
+            intl.borders[i].append(intl.block[i])
+            intl.all_sprites.add(intl.block[i])
